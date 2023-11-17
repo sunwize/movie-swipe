d40269e00c2224122fde27b3fe47479cd040094e
Maya <48359706+mayaesar@users.noreply.github.com>
2023-11-17 17:04:09 -0500

    add the api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,27 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 4000
+const fetch = require('node-fetch');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+express()
+.get("/movie" , async (req,res)=> {
+    const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+        }
+    };
+
+    const response = await fetch(url, options)
+    .then(res => res.json());
+
+    res.json(response.results);
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
